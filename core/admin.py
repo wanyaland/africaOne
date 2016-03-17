3abcea8105c8f1afb78ed1eebeaced991ed7a6ed
matovu <lsmatovu@gmail.com>
2016-03-17 19:48:52 +0300

    Merged with master. Admin core.

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from core.models import Category,Features,Business
-from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 
 admin.site.register(Category)
