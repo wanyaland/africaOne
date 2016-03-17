eb6f20a807b2e55538c879586dc0e08d088557f1
Wanyama <wanyaland@gmail.com>
2016-03-17 21:27:17 +0300

    wiring home admin page

--- admin/urls.py
+++ admin/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import patterns,url
-from core.views import *
-from djangoratings.views import AddRatingFromModel
-
+from . import views
 
 urlpatterns = patterns('',
-                       url(r'^$',index,name='home'),
+                       url(r'^$',views.index,name='home'),
                        )
