ad40166e1280dd31dbab7307102e1b0d4cabe702
Wanyama <wanyaland@gmail.com>
2016-03-19 09:41:25 +0300

    views

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.decorators import login_required
+        from django.contrib.auth.decorators import login_required
 from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth import logout
 from django.utils.decorators import method_decorator
