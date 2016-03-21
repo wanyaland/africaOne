7acff1058e42bd4dbc7818b84c8f74249b03da84
Wanyama <wanyaland@gmail.com>
2016-03-21 20:38:08 +0300

    adding business list

--- admin/views.py
+++ admin/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from core.models import Business,Category
+from django.views.generic import *
 
 # Create your views here.
 
 def index(request):
     return render(request,'admin/index.html')
+
+class BusinessList(ListView):
+    model = Business
+    queryset = Business.objects.all()
+
+
+
