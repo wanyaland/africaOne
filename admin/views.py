44e3cdbe4d5bb0e358c4892d639d962316ca7cd8
Wanyama <wanyaland@gmail.com>
2016-03-21 21:09:59 +0300

    home page admin

--- admin/views.py
+++ admin/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from core.models import Business,Category
 from django.views.generic import *
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 # Create your views here.
 
-def index(request):
-    return render(request,'admin/index.html')
 
 class BusinessList(ListView):
     model = Business
     queryset = Business.objects.all()
+    template_name = 'admin/index.html'
+    paginate_by = 50
+    
+
 
 
 
